c52150562eaee04f2470b5204de15e7361c870dc
Oleg Shatnyuk <o.shatnuk@gmail.com>
2020-10-26 11:28:21 +0300

    add settings activity/fragment without nav/validation

--- app/src/main/java/com/bmstu/shatnyuk/androidrk1/MainActivity.kt
+++ app/src/main/java/com/bmstu/shatnyuk/androidrk1/MainActivity.kt
@@ -2,10 +2,25 @@ package com.bmstu.shatnyuk.androidrk1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemId = item.itemId
+        if (itemId == R.id.action_refresh) {
+            // refresh here
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
